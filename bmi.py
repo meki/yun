c51588277c7acb659728c86b7314cc493c48f908
ssashida <ssashida@lexi.co.jp>
2020-04-28 10:26:56 +0900

    test2

--- bmi.py
+++ bmi.py
@@ -3,13 +3,13 @@
 
 from browser import document
 
-def calc_bmi():
-    weight = float(document["weight"].value)
-    height = float(document["height"].value)
+def calc_bmi(ctx):
+  weight = float(document["weight"].value)
+  height = float(document["height"].value)
 
-    bmi = str(weight/(height*height))
-    rslt = document["result"]
-    rslt.text = bmi
+  bmi = str(weight/(height*height))
+  rslt = document["result"]
+  rslt.text = bmi
 
 execute_btn = document["execute"]
 execute_btn.bind("click", calc_bmi)
